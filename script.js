c31201f9e85b95428d4362d49483dc3ff17904ce
Brian Nguyen <briannguyencoco@gmail.com>
2022-06-28 09:04:59 -0500

    created calculator interface along with operators functions

---
+++ script.js
@@ -0,0 +1,35 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(op, a, b) {
+    let result;
+    switch(op) {
+        case "*":
+            result = multiply(a, b);
+            break;
+        case "/":
+            result = divide(a, b);
+            break;
+        case "+":
+            result = add(a, b);
+            break;
+        case "-":
+            result = subtract(a, b);
+            break;
+        
+    }
+    return result;
+}
